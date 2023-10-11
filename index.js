fc093c3a4419a1c3155c254832cc57955201f23d
Βασιλης Καλυβας <bkalivas8@gmail.com>
2023-10-11 22:31:41 +0300

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: true,
         methods: ['GET', 'POST']
     }
 });
